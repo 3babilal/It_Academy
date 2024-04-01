b6ef681c2c61d6bb49001252aac6bfc569e45306
Bilal Layache <bilal.layache@gmail.com>
2024-04-01 14:36:22 +0200

    Respaldo 01/04/2024 Tasca 3 Nivel 2 Ejercicio 2

--- src/main/java/Sprint1/Tasca3/Nivel2/Ejercicio2/Menu.java
+++ src/main/java/Sprint1/Tasca3/Nivel2/Ejercicio2/Menu.java
@@ -1,4 +1,21 @@
 package Sprint1.Tasca3.Nivel2.Ejercicio2;
 
+import Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant;
+
+import java.util.HashSet;
+
 public class Menu {
+    public static void main(String[] args) {
+        HashSet<Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant> restaurants = new HashSet<>();
+        Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant r1 = new Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant("Dolce Vita", 7);
+        Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant r2 = new Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant("Dolce Vita", 7);
+        Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant r3 = new Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant("La italiana", 7);
+        Sprint1.Tasca3.Nivel2.Ejercicio1.Restaurant r4 = new Restaurant("La italiana", 2);
+        restaurants.add(r1);
+        restaurants.add(r2);
+        restaurants.add(r3);
+        restaurants.add(r4);
+
+        System.out.println(restaurants);
+    }
 }
