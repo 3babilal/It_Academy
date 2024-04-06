5fe4dd0e565e52072e270b75e13d13f3ff5b0c75
Bilal Layache <bilal.layache@gmail.com>
2024-04-06 10:19:10 +0200

    Mejoras ejercicios 06/04/2024

--- src/main/java/Sprint1/Tasca6/Nivel1/Ejercicio2/GenericMethods.java
+++ src/main/java/Sprint1/Tasca6/Nivel1/Ejercicio2/GenericMethods.java
@@ -2,6 +2,6 @@ package Sprint1.Tasca6.Nivel1.Ejercicio2;
 
 public class GenericMethods {
     public static <T> void generic(T one, T two, T three ){
-        System.out.println("Uno :" + one + "Dos :" + "Tres :" + three);
+        System.out.println("Uno :" + one + " Dos :" + " Tres :" + three);
     }
 }
